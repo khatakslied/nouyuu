3694366b11bc2bb3c1b3dfaef657391181e59c43
matias acuna <agroang@gmail.com>
2021-11-22 19:10:10 +0900

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :crops, only: [:index, :show]
+  resources :gardens, only: [:show, :index] do
+    resources :plots, only: [:new, :create]
+  end
 end
