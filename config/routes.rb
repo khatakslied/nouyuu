5e7e8e9f4119e0f8842566bd4dc52e9c8821e88b
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-11-23 22:39:43 +0900

    Add style on the sign up forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :crops, only: [:index, :show]
+  resources :crops, only: [:index, :show] do
+     member do
+      post 'toggle_favorite', to: "crops#toggle_favorite"
+    end
+  end
   resources :gardens, only: [:show, :index, :new, :create] do
     resources :plots, only: [:new, :create]
   end
