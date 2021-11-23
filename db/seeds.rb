fcf1f9aa633ae76e49b5da35a2b39b9fdeb81349
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-23 15:47:43 +0900

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+serialized_crops = File.read("db/crops.JSON")
+crops_list = JSON.parse(serialized_crops)
+crops_list["crops"].each do |crop|
+  Crop.create!(crop)
+end
