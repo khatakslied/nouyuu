2eeaf0a7f3c3c0c28b4c2ce964e55658cda37dc1
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-23 18:06:53 +0900

    Add gardens#index, gardens#new and gardens#create actions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-serialized_crops = File.read("db/crops.JSON")
+
+puts "Deleting crops..."
+Crop.delete_all
+puts "Deleted crops."
+
+puts "Adding crops..."
+serialized_crops = File.read("db/crops.json")
 crops_list = JSON.parse(serialized_crops)
 crops_list["crops"].each do |crop|
   Crop.create!(crop)
 end
+puts "Added #{Crop.count} crops."
