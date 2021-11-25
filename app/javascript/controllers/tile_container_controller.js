13ecc830cffec6c2322665b8306959a708fec4a6
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-25 18:07:14 +0900

    Make plots selectable in gardens#show.

--- app/javascript/controllers/tile_container_controller.js
+++ app/javascript/controllers/tile_container_controller.js
@@ -6,6 +6,14 @@ export default class extends Controller {
   static targets = ["tile", "input", "link", "form"]
 
   changeClass(event) {
+    if (event.currentTarget.dataset.plotId) {
+      this.selectPlot(event)
+    } else {
+      this.selectFree(event)
+    }
+  }
+
+  selectFree(event) {
     event.currentTarget.classList.toggle('selected')
     if (idSet.has(event.currentTarget.id)){
       idSet.delete(event.currentTarget.id)
@@ -14,6 +22,14 @@ export default class extends Controller {
     };
     const idArray = new Array(...idSet)
     this.inputTarget.value = idArray
+  }
+
+  selectPlot(event) {
+    // Can we do this with Stimulus?
+    const tiles = document.querySelectorAll(`div[data-plot-id='${event.currentTarget.dataset.plotId}']`)
+    tiles.forEach(tile => {
+      tile.classList.toggle('selected')
+    });
   }
 
   displayForm() {
