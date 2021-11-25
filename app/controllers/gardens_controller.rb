56e4362bdb666b4cf2bfd2ef07c95a8568a3b421
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-11-25 13:45:48 +0900

    Added open weather api

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,4 +1,9 @@
+require 'uri'
+require 'net/http'
+
 class GardensController < ApplicationController
+  before_action :weather_call
+
   def index
     @gardens = policy_scope(Garden).order(created_at: :desc)
   end
@@ -26,6 +31,16 @@ class GardensController < ApplicationController
     else
       render :new
     end
+  end
+
+  def weather_call
+    key = "409db7da513ae73d18041b554926cb9e"
+    uri = URI('https://api.openweathermap.org/data/2.5/weather')
+    params = { units: 'metric', q: "Tokyo", appid: key }
+    uri.query = URI.encode_www_form(params)
+
+    res = Net::HTTP.get_response(uri)
+    @weather_response = JSON.parse(res.body)['weather']['icon']
   end
 
   private
