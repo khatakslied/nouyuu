38a3c9f543d0b95cfb25bc2bc47947478f4ef9bb
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-11-26 10:42:13 +0900

    Intermidiary commit

--- app/javascript/controllers/select_crops_controller.js
+++ app/javascript/controllers/select_crops_controller.js
@@ -1,14 +1,16 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["crop"]
+  static targets = ["basket"]
 
   connect() {
-    console.log(this.cropTarget)
+    console.log(this.cropTargets)
   }
 
   mark(event) {
-    event.preventDefault()
-    event.currentTarget.classList.toggle('favorited')
+    event.preventDefault();
+    this.cropTargets.forEach((element) => {
+      element.classList.toggle('favorited-card')
+    });
   }
 }
