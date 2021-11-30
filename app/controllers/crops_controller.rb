d0dac00015c13229c6e1f1dade071d2fb6e4b966
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-30 14:27:30 +0900

    Add harvest indicator.

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -2,15 +2,6 @@ class CropsController < ApplicationController
   def index
     @crops = policy_scope(Crop).all
     @favorite_crops = current_user.favorites_by_type('Crop').count
-
-    # if params[:query].present?
-    #   @cropes = Crop.where('name ILIKE ?', "%#{params[:query]}%")
-    # end
-
-    # respond_to do |format|
-    #   format.html # default Rails behavior (render :index)
-    #   format.text { render partial: 'cropes/crop', locals: { crops: @crops }, formats: [:html] }
-    # end
   end
 
   def show
