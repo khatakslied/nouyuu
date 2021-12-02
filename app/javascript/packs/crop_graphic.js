59472a14bc9d1d647474f688a1271fb8f544856a
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-12-03 01:12:57 +0900

    Save unused 3js.

--- app/javascript/packs/crop_graphic.js
+++ app/javascript/packs/crop_graphic.js
@@ -1,63 +1,58 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
+const assetMapping = {
+    "Amaranthaceae": "/amaranthaceae.glb",
+    "Brassicaceae": "/brassicaceae.glb",
+    "Cucurbitaceae": "/cucurbitaceae.glb",
+    "Ericaceae": "/ericaceae.glb",
+    "Liliaceae": "/liliaceae.glb",
+    "Solanaceae": "/solanaceae.glb",
+    "Asparagaceae": "/herb.glb",
+    "Asteraceae": "/herb.glb",
+    "Convolvulaceae": "/herb.glb",
+    "Fabaceae": "/herb.glb",
+    "Grossulariaceae": "/herb.glb",
+    "Lamiaceae": "/herb.glb",
+    "Malvaceae": "/herb.glb",
+    "Passifloraceae": "/herb.glb",
+    "Poaceae": "/herb.glb",
+    "Rosaceae": "/herb.glb",
+    "Umbelliferae": "/herb.glb",
+    'Zingiberaceae': "/herb.glb"
+}
+
 export default function() {
 
-  const tiles = document.querySelectorAll(`div[data-crop-name='Tomato']`);
+
+  const tiles = document.querySelectorAll(``);
   tiles.forEach(tile => {
 
+    const renderer = new THREE.WebGLRenderer( { alpha: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    const scene = new THREE.Scene();
+    let camera = new THREE.PerspectiveCamera( 1, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const light = new THREE.AmbientLight( 0x404040, 5 );
 
-    ////////////START
-      const scene = new THREE.Scene();
-      let camera = new THREE.PerspectiveCamera( 1, window.innerWidth / window.innerHeight, 0.1, 1000 );
-      let directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
-      const light = new THREE.AmbientLight( 0x404040, 5 );
-      const renderer = new THREE.WebGLRenderer( { alpha: true } );
-      renderer.setSize( window.innerWidth, window.innerHeight );
+    if (assetMapping[tile.dataset.cropFamily]) {
 
-      // camera.position.set(0, 0, 50);
       const loader = new GLTFLoader();
 
-      loader.load('/solanaceae.glb',
+      loader.load(assetMapping[tile.dataset.cropFamily],
         ( gltf ) => {
-        console.log(gltf)
         scene.add( gltf.scene );
         scene.add( light );
         camera = gltf.cameras[0];
-        // scene.background = new THREE.set(0xffffff, 0);
       });
 
+    }
 
-      ////////// END
       tile.appendChild( renderer.domElement )
       const animate = function () {
         requestAnimationFrame( animate );
         renderer.render( scene, camera );
       };
       animate();
-
     });
-
-  // loader.load('/testcrop1.gltf',
-	// // called when the resource is loaded
-	// ( gltf ) => {
-
-  //   scene.add( gltf.scene );
-  //   scene.background = new THREE.set(0xffffff, 0);
-  //   scene.clone(true)
-  //   // scene.add(light);
-	// },
-  // 	// called while loading is progressing
-	// function ( xhr ) {
-
-	// 	console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-	// },
-	// // called when loading has errors
-	// function ( error ) {
-
-	// 	console.log( 'An error happened' );
-
-	// });
 
 }
